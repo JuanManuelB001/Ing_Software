56e2eda24a9bb0fd5f079f42e1058a3d77e41631
JuanManuelB001 <134808762+JuanManuelB001@users.noreply.github.com>
2023-05-31 13:29:02 -0500

    Delete spring.crud directory

--- spring.crud/src/main/java/com/example/demo/repositorio/iuser.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.user;
-
-
-public interface iuser  extends JpaRepository<user,Long>{
-
-}
